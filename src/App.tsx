647e5f6c64bdd635ee9a7db18d13d12c540384da
Toro_Unit (Hiroshi Urabe) <mail@torounit.com>
2019-01-07 12:31:34 +0900

    add ts setting and material-ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,15 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import { AppBar, Toolbar, Typography } from "@material-ui/core"
+import withTheme from "./withTheme"
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+const App: React.SFC = () => (
+  <div className="App">
+    <AppBar>
+      <Toolbar>
+        <Typography  color="inherit" >App</Typography>
+      </Toolbar>
+    </AppBar>
+  </div>
+)
 
-export default App;
+export default withTheme(App)
