fda571847908fe002911cb6f2bc7b5a6bea75d56
Toro_Unit (Hiroshi Urabe) <mail@torounit.com>
2019-01-20 06:18:59 +0900

    use .env for firebase.

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,13 +1,17 @@
 import * as firebase from "firebase/app"
 import "firebase/database"
 
-const app = firebase.initializeApp({
-  apiKey: "AIzaSyBIg1CM-15Ag74cwnB9BaJG3z_pFnr8BX0",
-  authDomain: "torounit-firebase-chat.firebaseapp.com",
-  databaseURL: "https://torounit-firebase-chat.firebaseio.com",
-  projectId: "torounit-firebase-chat",
-  storageBucket: "torounit-firebase-chat.appspot.com",
-  messagingSenderId: "646034935690",
-})
+const {
+  REACT_APP_FIREBASE_API_KEY: apiKey,
+  REACT_APP_FIREBASE_AUTH_DOMAIN: authDomain,
+  REACT_APP_FIREBASE_DATABASE_URL: databaseURL,
+  REACT_APP_FIREBASE_PROJECT_ID: projectId,
+  REACT_APP_FIREBASE_STORAGE_BUCKET: storageBucket,
+  REACT_APP_FIREBASE_MESSAGING_SENDER_ID: messagingSenderId,
+} = process.env
+
+const options = { apiKey, authDomain, databaseURL, projectId, storageBucket, messagingSenderId }
+
+const app = firebase.initializeApp(options)
 
 export const database = app.database()
